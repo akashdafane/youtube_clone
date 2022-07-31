4602e6a271e6a5fa34dad6202aa437aa1b015bca
akashdafane-btc <akash.dafane@tntra.io>
2022-07-31 19:41:38 +0530

    added watch later functionality in search result

--- src/pages/searchResult/searchResultPage.js
+++ src/pages/searchResult/searchResultPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { AddChanelId } from '../../actions/addHistory';
 import Videos from './videos';
 
 const SearchResultPage = () => {
@@ -9,9 +11,11 @@ const SearchResultPage = () => {
   );
 
   let navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleNavigation = (id) => {
     navigate(`/watch/${id}`);
+    dispatch(AddChanelId(id));
   };
 
   return (
--- src/pages/searchResult/videos.js
+++ src/pages/searchResult/videos.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import { VideoPlayer } from '../../components';
+import React, { useState, useRef } from 'react';
+import { VideoPlayer, Popover } from '../../components';
+import { useDispatch } from 'react-redux';
+import { WatchLaterData } from '../../actions/watchLater';
+import {
+  MoreVertIcon,
+  WatchLaterOutlinedIcon,
+} from '../../constants/iconConstants';
 import '../../styles/videos.css';
 
-
 const Videos = ({ id, title, description, handleNavigation }) => {
+  const [show, setShow] = useState(false);
+  const [target, setTarget] = useState(null);
+  const ref = useRef(null);
+
+  const dispatch = useDispatch();
+
+  const handleToggle = (event) => {
+    setShow(!show);
+    setTarget(event.target);
+  };
+
+  const handleNavigate = (id) => {
+    setShow(false);
+    dispatch(WatchLaterData(id, title));
+  };
   return (
     <div>
       <div className="vid-card" onClick={() => handleNavigation(id)}>
@@ -18,8 +38,17 @@ const Videos = ({ id, title, description, handleNavigation }) => {
           <div className="sub-title"> 27M views 1 year ago</div>
           <div className="text">{description}</div>
         </div>
-        {/* <img src={MoreIcon} className="sip-menu-icon" alt="Sip" /> */}
       </div>
+      <Popover
+        handleToggle={handleToggle}
+        show={show}
+        target={target}
+        ref={ref}
+        handleNavigate={() => handleNavigate(id)}
+        icon={<MoreVertIcon />}
+        listIcon={<WatchLaterOutlinedIcon />}
+        listItemName={'Watch Later'}
+      />
     </div>
   );
 };
